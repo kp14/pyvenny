676d9dc1a60e064c9e19bc63c35f839455160d3c
Adrian Green <harvardsommer@gmx.net>
2015-05-19 16:58:02 +0100

    Made py3 ready ... I think

--- pyvenny/__init__.py
+++ pyvenny/__init__.py
@@ -2,7 +2,7 @@
 
 from decimal import Decimal
 from jinja2 import Environment, PackageLoader
-    
+
 
 def fifteen_normalized_sections(a, b, c, d):
     union = Decimal(len(set.union(a,b,c,d)))
@@ -29,14 +29,65 @@ def fifteen_normalized_sections(a, b, c, d):
              'AiBiCiD': "%0.2f" % (len(set.intersection(a,b,c,d))/union) }
 
 
+def fifteen_sections(a, b, c, d):
+#    union = Decimal(len(set.union(a,b,c,d)))
+
+    # get sizes for regions delimited by set areas
+    # _ is for - as in substract, i is for intersection, u is for union
+    return { 'A_BuCuD': str(len(a - set.union(b,c,d))),
+             'B_AuCuD': str(len(b - set.union(a,c,d))),
+             'C_AuBuD': str(len(c - set.union(a,b,d))),
+             'D_AuBuC': str(len(d - set.union(a,b,c))),
+
+             'AiB_CuD': str(len(set.intersection(a,b) - set.union(c,d))),
+             'AiC_BuD': str(len(set.intersection(a,c) - set.union(b,d))),
+             'AiD_BuC': str(len(set.intersection(a,d) - set.union(b,c))),
+             'BiC_AuD': str(len(set.intersection(b,c) - set.union(a,d))),
+             'BiD_AuC': str(len(set.intersection(b,d) - set.union(a,c))),
+             'CiD_AuB': str(len(set.intersection(c,d) - set.union(a,b))),
+
+             'AiBiC_D': str(len(set.intersection(a,b,c) - d)),
+             'BiCiD_A': str(len(set.intersection(b,c,d) - a)),
+             'AiCiD_B': str(len(set.intersection(a,c,d) - b)),
+             'AiBiD_C': str(len(set.intersection(a,b,d) - c)),
+
+             'AiBiCiD': str(len(set.intersection(a,b,c,d))) }
+
+
+def fifteen_subsets(a, b, c, d):
+    union = Decimal(len(set.union(a,b,c,d)))
+
+    # get sizes for regions delimited by set areas
+    # _ is for - as in substract, i is for intersection, u is for union
+    return { 'A_BuCuD': a - set.union(b,c,d),
+             'B_AuCuD': b - set.union(a,c,d),
+             'C_AuBuD': c - set.union(a,b,d),
+             'D_AuBuC': d - set.union(a,b,c),
+
+             'AiB_CuD': set.intersection(a,b) - set.union(c,d),
+             'AiC_BuD': set.intersection(a,c) - set.union(b,d),
+             'AiD_BuC': set.intersection(a,d) - set.union(b,c),
+             'BiC_AuD': set.intersection(b,c) - set.union(a,d),
+             'BiD_AuC': set.intersection(b,d) - set.union(a,c),
+             'CiD_AuB': set.intersection(c,d) - set.union(a,b),
+
+             'AiBiC_D': set.intersection(a,b,c) - d,
+             'BiCiD_A': set.intersection(b,c,d) - a,
+             'AiCiD_B': set.intersection(a,c,d) - b,
+             'AiBiD_C': set.intersection(a,b,d) - c,
+
+             'AiBiCiD': set.intersection(a,b,c,d)}
+
+
+
 def render_four_set_venn( a, b, c, d, titles={'A': 'A',
                                               'B': 'B',
                                               'C': 'C',
                                               'D': 'D'} ):
-    sections = fifteen_normalized_sections( a, b, c, d )
-                          
+    sections = fifteen_sections( a, b, c, d )
+
     env = Environment(loader=PackageLoader('pyvenny', 'templates'))
     template = env.get_template('four_set_venn.svg')
-    
-    return template.render( dict(sections.items() + titles.items()) )
+
+    return template.render( dict(sections.items() | titles.items()) )
 
